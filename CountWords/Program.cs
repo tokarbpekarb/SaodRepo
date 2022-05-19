732c962e31d58fdee56ac278cf890ac93207a03e
xrusha2003 <zns.20@uni-dubna.ru>
2022-05-19 14:53:17 +0300

    mediana

--- CountWords/Program.cs
+++ CountWords/Program.cs
@@ -8,34 +8,61 @@ namespace CountWords
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void nataliaTest()
         {
             var loader = new Loader();
             var words = loader.Load("../../../big.txt");
             Console.WriteLine("Всего слов: " + words.Count);
 
-               // Всего слов: 10611980
-               // Уникальных: 525090
-               // the 564373
+            // Всего слов: 10611980
+            // Уникальных: 525090
+            // the 564373
 
-            
-            var dict = new SortedDictionary<string, int>();
-            //var dict = new Dictionary<string, int>();
-            //var dict = new ВашКонтейнер<string, int>();
-            foreach(var word in words)
+            System.Diagnostics.Stopwatch watch;
+            List<long> times = new List<long>();
+
+            long elapsedMs;
+
+            for (int i = 0; i < 5; i++)
             {
-                if(dict.ContainsKey(word)) // log(n)
-                        ++dict[word];      // log(n)
-                else
-                        dict.Add(word, 1);
-            }
-            Console.WriteLine("Уникальных: " + dict.Count);
+                watch = System.Diagnostics.Stopwatch.StartNew();
 
-             string str = "the";
-             if(dict.ContainsKey(str))
-                Console.WriteLine(str + " " + dict[str]);
-             else
-                Console.WriteLine(str + " 0");
+
+                var dict = new Dictionary<string, int>();
+                //var dict = new Dictionary<string, int>();
+                //var dict = new ВашКонтейнер<string, int>();
+                foreach (var word in words)
+                {
+                    if (dict.ContainsKey(word)) // log(n)
+                        ++dict[word];      // log(n)
+                    else
+                        dict.Add(word, 1);
+                }
+                Console.WriteLine("Уникальных: " + dict.Count);
+
+                watch.Stop();
+                elapsedMs = watch.ElapsedMilliseconds;
+                times.Add(elapsedMs);
+
+
+            }
+            var mediana = new mediana();
+
+            var result = mediana.calc_mean_median_stddev(times);
+
+            Console.WriteLine(result);
+
+
         }
+
+        static void Main(string[] args)
+        {
+            nataliaTest();
+        }
+
+
+
+
     }
 }
