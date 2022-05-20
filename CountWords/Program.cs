cac611a382430cf7587e90f92c9e5adf546a5802
tokarbpekarb <50523349+tokarbpekarb@users.noreply.github.com>
2022-05-20 14:58:31 +0300

    реализация задания 9 . (подсчёт вхождений AVL)

--- CountWords/Program.cs
+++ CountWords/Program.cs
@@ -19,33 +19,44 @@ namespace CountWords
             //Console.WriteLine(tree.Print());
             //Console.ReadKey();
 
+            var loader = new Loader();
+            var words = loader.Load("../../../big.txt");
+            Console.WriteLine("Всего слов: " + words.Count);
 
-            //    var loader = new Loader();
-            //    var words = loader.Load("../../../big.txt");
-            //    Console.WriteLine("Всего слов: " + words.Count);
+            // Всего слов: 10611980
+            // Уникальных: 525090
+            // the 564373
 
-            //       // Всего слов: 10611980
-            //       // Уникальных: 525090
-            //       // the 564373
+            //реализация через AVL дерево
+            WordsCountAVL tree = new WordsCountAVL();
+            foreach (var word in words)
+            {
+                if (tree.ContainsKey(word))
+                    ++tree[word];
+                else
+                    tree.Add(word, 1);
+            }
+            Console.WriteLine("УникальныхЖ " + tree.Count);
 
 
-            //    var dict = new SortedDictionary<string, int>();
-            //    //var dict = new Dictionary<string, int>();
-            //    //var dict = new ВашКонтейнер<string, int>();
-            //    foreach(var word in words)
-            //    {
-            //        if(dict.ContainsKey(word)) // log(n)
-            //                ++dict[word];      // log(n)
-            //        else
-            //                dict.Add(word, 1);
-            //    }
-            //    Console.WriteLine("Уникальных: " + dict.Count);
+            //var dict = new SortedDictionary<string, int>();
+            //var dict = new Dictionary<string, int>();
+            //var dict = new ВашКонтейнер<string, int>();
+            //foreach (var word in words)
+            //{
+            //    if (dict.ContainsKey(word)) // log(n)
+            //        ++dict[word];      // log(n)
+            //    else
+            //        dict.Add(word, 1);
+            //}
+            //Console.WriteLine("Уникальных: " + dict.Count);
 
-            //     string str = "the";
-            //     if(dict.ContainsKey(str))
-            //        Console.WriteLine(str + " " + dict[str]);
-            //     else
-            //        Console.WriteLine(str + " 0");
+            //string str = "the";
+            //if (dict.ContainsKey(str))
+            //    Console.WriteLine(str + " " + dict[str]);
+            //else
+            //    Console.WriteLine(str + " 0");
+            Console.ReadKey();
         }
     }
 }
